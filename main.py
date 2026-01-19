b2051a7a20a4147e0edccf2360ad754c6dd2bbeb
Aaku26 <akankshashinde9117@gmail.com>
2026-01-19 23:54:16 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,96 @@
+import numpy as np
+from operationclass import IntArray
+
+def productivity_of_company(order, data_frame):
+    return np.sum(data_frame[order])
+
+
+def max_productivity(data_frame):
+    i= 0
+    best_company = i + 1
+    num_of_products = 0
+
+    for i in range(len(data_frame)):
+        result = productivity_of_company(i, data_frame)
+        if result > num_of_products:
+            num_of_products = result
+            best_company = i + 1
+
+    print(f"The best company is the {best_company}.company with {num_of_products}")
+
+def min_productivity(data_frame):
+    i = 0
+    worst_company = i +1 
+    num_of_products =productivity_of_company(0, data_frame)
+
+    for  i in range(len(data_frame)):
+        result = productivity_of_company(i , data_frame)
+        if result  <= num_of_products:
+            num_of_products = result
+            worst_company = i + 1
+
+    print(f"The worst company is the {worst_company}. company with {num_of_products}")                   
+
+    
+
+
+def file_handling():
+    rows = []
+
+    with open("company.txt", "r") as file:
+        for line in file:
+            values = line.strip().split(',')
+            clean_row = []
+
+            for val in values:
+                try:
+                    clean_row.append(int(val))
+                except ValueError:
+                    print(f"Invalid value skipped: {val}")
+
+            if clean_row:
+                rows.append(clean_row)
+
+    data_frame = np.array(rows, dtype=object)
+    return data_frame
+
+
+def mean_productivity(data_frame):
+    for i in range(len(data_frame)):
+        average = np.mean(data_frame)
+        print(f"On average, one employee from {i + 1}. company produces {average} products")
+
+    sum_products  = 0
+    num_employee = 0
+
+    for row in data_frame:
+        for employee in row:
+            num_employee +=1
+
+    for row in data_frame:
+        row_sum = np.sum(data_frame[row])
+        sum_products += row_sum 
+
+    total_mean = sum_products / num_employee
+    print(f"Total mean of entite monopoly is{total_mean} per employee")               
+
+
+def main():
+    data_frame = file_handling()
+
+    print("Complete data frame:")
+    print(data_frame)
+    print("Type:", type(data_frame))
+
+
+    first_branch = IntArray(np.array(data_frame[0], dtype=int))
+    first_branch.display()
+    first_branch.salary()
+    first_branch.show_data()
+
+    max_productivity(data_frame)
+    min_productivity(data_frame)
+    mean_productivity(data_frame)
+
+
+main()
